b39486b6b9e527e7e573cfe6053f18c760d9f2ce
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2025-07-28 22:03:01 +0600

    api updated

--- src/api/shopify.js
+++ src/api/shopify.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:3001/api";
+const API = "https://shopify-analytics-back.netlify.app/api";
+// const API = "http://localhost:3001/api";
 
 export const getOrders = () => axios.get(`${API}/orders`);
 export const getProducts = () => axios.get(`${API}/products`);

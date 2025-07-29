2bf8747e231b2dfdaaad75ee99e715cf5d01b84c
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2025-07-29 20:09:06 +0600

    vercel update

--- src/api/shopify.js
+++ src/api/shopify.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const API = "https://analytics-dashboard-back-esea.vercel.app/api";
-// const API = "https://shopify-analytics-back.netlify.app/api";
-const API = "http://localhost:3001/api";
+const API = "https://analytics-dashboard-backend-plum.vercel.app/api";
+// const API = "http://localhost:3001/api";
 
 export const getOrders = () => axios.get(`${API}/orders`);
 export const getProducts = () => axios.get(`${API}/products`);
